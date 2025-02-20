c3be2e343b87785a82e22491b22b6bfd38d09505
aliakborswe <aliakborswe@gmail.com>
2025-02-20 16:33:35 +0600

    add: create a new task api done

---
+++ src/controllers/taskController.js
@@ -0,0 +1,12 @@
+const taskModel = require('../models/TaskModel');
+
+// Add a new task
+export const createNewTask = ("/", async (req, res) => {
+  try {
+    const newTask = new Task(req.body);
+    await newTask.save();
+    res.status(201).json(newTask);
+  } catch (error) {
+    res.status(500).json({ message: "Error creating task", error });
+  }
+});
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController')
+const taskController = require('../controllers/taskController')
 
+// user routes
 router.post("/users", userController.createNewUser);
+
+// task routes
+router.post("/tasks", taskController.createNewTask);  
 
 module.exports = router;
